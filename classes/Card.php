a9f7ada514764ac45225ab536697164e00069638
UrbanoG2 <giuseppe.16.urbano@gmail.com>
2022-02-16 14:57:09 +0100

    init

---
+++ classes/Card.php
@@ -0,0 +1,75 @@
+<?php
+
+class Card 
+{
+    protected $cvv;
+    protected $expDate;
+    protected $number;
+
+    public function __construct($cvv, $expDate, $number)
+    {
+        $this->cvv = $cvv;
+        $this->expDate = $expDate;
+        $this->number = $number;
+    }
+
+    /**
+     * Get the value of cvv
+     */ 
+    public function getCvv()
+    {
+        return $this->cvv;
+    }
+
+    /**
+     * Set the value of cvv
+     *
+     * @return  self
+     */ 
+    public function setCvv($cvv)
+    {
+        $this->cvv = $cvv;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of expDate
+     */ 
+    public function getExpDate()
+    {
+        return $this->expDate;
+    }
+
+    /**
+     * Set the value of expDate
+     *
+     * @return  self
+     */ 
+    public function setExpDate($expDate)
+    {
+        $this->expDate = $expDate;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of number
+     */ 
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * Set the value of number
+     *
+     * @return  self
+     */ 
+    public function setNumber($number)
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+}
---
+++ classes/Product.php
@@ -0,0 +1,96 @@
+<?php 
+
+class Product 
+{
+    protected $name;
+    protected $price;
+    protected $type;
+    protected $barcode;
+
+    public function __construct($name, $price, $type)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->type = $type;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of price
+     */ 
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set the value of price
+     *
+     * @return  self
+     */ 
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of type
+     */ 
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set the value of type
+     *
+     * @return  self
+     */ 
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of barcode
+     */ 
+    public function getBarcode()
+    {
+        return $this->barcode;
+    }
+
+    /**
+     * Set the value of barcode
+     *
+     * @return  self
+     */ 
+    public function setBarcode($barcode)
+    {
+        $this->barcode = $barcode;
+
+        return $this;
+    }
+}

1b82c0b4f6f14f9e06dd05546281a79f16cffd57
UrbanoG2 <giuseppe.16.urbano@gmail.com>
2022-02-16 15:06:26 +0100

    Dump working

--- Index.php
+++ Index.php
@@ -2,3 +2,8 @@
 
 require_once __DIR__ . "/classes/User.php";
 require_once __DIR__ . "/classes/Product.php";
+
+$user = new User ("Giuseppe", "Urbano", new Card(851, "03/35", "2366594
+61168496" ));
+
+var_dump($user);
--- classes/User.php
+++ classes/User.php
@@ -17,7 +17,6 @@ class User
         $this->name = $name;
         $this->lastname = $lastname;
         $this->setCard($card);
-
     }
 
     /**
